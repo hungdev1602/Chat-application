085f9f0998fd082bfc11961d8e310d01a051179b
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-11-21 00:47:40 +0300

    hoàn thiện middleware user khi đăng nhập, trả về user cho FE để hiện thị đăng xuất/đăng ký

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,11 @@
 const chatRoute = require("./chat.route")
 const userRoute = require("./user.route")
 
+const userMiddleware = require("../../middlewares/client/user.middleware")
+
 module.exports.index = (app) => {
+  app.use(userMiddleware.infoUser)
+
   app.use("/", chatRoute)
 
   app.use('/user', userRoute)
