bcaa80242b0c9f58b70e8c60aa29d3c56444178a
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-11-20 23:48:28 +0300

    Tính năng đăng nhập

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -39,3 +39,42 @@ module.exports.registerPost = async (req, res) => {
 
   res.redirect("/")
 }
+
+module.exports.login = (req, res) => {
+  res.render("client/pages/user/login", {
+    pageTitle: "Đăng nhập"
+  })
+}
+
+module.exports.loginPost = async (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  const user = await User.findOne({
+    email: email,
+    deleted: false
+  })
+
+  if(!user){
+    req.flash("error", "Email không tồn tại trong hệ thống")
+    res.redirect("back")
+    return
+  }
+
+  if(md5(password) !== user.password){
+    req.flash("error", "Sai mật khẩu")
+    res.redirect("back")
+    return
+  }
+
+  if(user.status !== "active"){
+    req.flash("error", "Tài khoản bị khoá")
+    res.redirect("back")
+    return
+  }
+
+  res.cookie("tokenUser", user.token)
+  req.flash("success", "Đăng nhập thành công")
+
+  res.redirect("/")
+}
