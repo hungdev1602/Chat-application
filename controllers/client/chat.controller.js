8cc71b2e58e0be7f68ac39d44e7b9e2b3ba170e2
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-11-21 16:27:48 +0300

    Tính năng chat cơ bản, lưu tin nhắn vào database và f5 lại web để đọc

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,9 +1,38 @@
-module.exports.index = (req, res) => {
-  _io.on("connection", (socket) => {
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
+
+module.exports.index = async (req, res) => {
+  _io.once("connection", (socket) => {
     console.log("Có 1 user kết nối!", socket.id);
+
+    // Lấy tin nhắn từ client gửi cho server, sau đó lưu vào database
+    socket.on("CLIENT_SEND_MESSAGE", async (data) => {
+      const dataChatForSave = {
+        userId: res.locals.user.id,
+        content: data.message
+      }
+
+      // lưu tin nhắn vào database
+      const newChat = new Chat(dataChatForSave)
+      await newChat.save();
+    })
   });
 
+  // Lấy ra tin nhắn mặc định từ database
+  const chats = await Chat.find({
+    deleted: false
+  })
+
+  for (const chat of chats) {
+    const infoUser = await User.findOne({
+      _id: chat.userId
+    })
+    
+    chat.fullName = infoUser.fullName
+  }
+
   res.render("client/pages/chat/index", {
-    pageTitle: "Chat App"
+    pageTitle: "Chat App",
+    chats: chats
   });
 }
--- public/js/script.js
+++ public/js/script.js
@@ -9,3 +9,20 @@ if(alertMessage){
 
 // Socket.io
 var socket = io();
+
+const formChat = document.querySelector(".inner-form");
+if(formChat){
+  formChat.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // Lấy được tin nhắn từ form và gửi cho server tin nhắn đó thông qua socket
+    const message = formChat.content.value
+    const data = {
+      message: message
+    }
+    socket.emit("CLIENT_SEND_MESSAGE", data)
+    formChat.content.value = ""
+  });
+}
+
+// End Socket.io
